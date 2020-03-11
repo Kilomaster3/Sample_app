edd8348f0cf437dda146cea072e6d55309728ed7
Kilomaster_3 <hromalex.2011@gmail.com>
2020-03-11 17:33:00 +0300

    Add new Test and Password

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_140935) do
+ActiveRecord::Schema.define(version: 2020_03_11_142833) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
