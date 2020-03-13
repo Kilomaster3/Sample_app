fed9b6acbc3d074ed2558d7f1a471a6e685b7594
Kilomaster_3 <hromalex.2011@gmail.com>
2020-03-13 20:57:50 +0300

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = 'Welcome to the Sample App!'
-      redirect_to root_url, notice: 'Signed up!'
+      redirect_to @user
     else
       render 'new'
     end
@@ -20,6 +21,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,7 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: '',
+      post users_path, params: { user: { name: ' ',
                                          email: 'user@invalid',
                                          password: 'foo',
                                          password_confirmation: 'bar' } }
@@ -15,5 +15,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'li', "Name can't be blank"
     assert_select 'li', 'Email is invalid'
     assert_select 'li', "Password confirmation doesn't match Password"
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
