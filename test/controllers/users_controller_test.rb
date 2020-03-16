167b27b88fe00ec407b937691f12df8400336810
Kilomaster_3 <hromalex.2011@gmail.com>
2020-03-16 21:25:25 +0300

    User index Test and Adding 30 new users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -21,12 +21,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
-  test "should get new" do
+  test 'should get new' do
     get signup_path
     assert_response :success
   end
 
-  test "should redirect index when not logged in" do
+  test 'should redirect index when not logged in' do
     get users_path
     assert_redirected_to login_url
   end
