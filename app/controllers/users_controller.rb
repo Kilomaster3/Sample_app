5a11cacc493ab627af430b2685595db3e5093214
Kilomaster_3 <hromalex.2011@gmail.com>
2020-03-16 20:58:49 +0300

    Modified User Page, Root Update Test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index 
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -23,7 +30,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Profile updated'
       redirect_to @user
@@ -31,8 +37,6 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
-  before_action :logged_in_user, only: [:edit, :update]
 
   private
 
@@ -43,8 +47,15 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = 'Please log in.'
       redirect_to login_url
     end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
