11e4d01d05fb749a9819a62e5376242e9dd8b3ea
Kilomaster_3 <hromalex.2011@gmail.com>
2020-03-16 18:56:43 +0300

    Fix Bags

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,10 +32,19 @@ class UsersController < ApplicationController
     end
   end
 
+  before_action :logged_in_user, only: [:edit, :update]
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
   end
 end
