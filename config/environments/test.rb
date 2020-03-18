3c3d44a5ce98a8fc010abddda3f8a509ef0ccf75
Kilomaster_3 <hromalex.2011@gmail.com>
2020-03-18 17:24:31 +0300

    Add account activation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,7 +33,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
-
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
@@ -41,7 +40,6 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'example.com' }
-  
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
